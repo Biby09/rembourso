179596451ea9b7f4836002dae3bf0aecdc4a44e5
Florian Lovis <lovis.florian@gmail.com>
2026-09-21 20:58:05 +0200

    ajout de la gestion des exceptions serveur avec envoi d'un email au webmaster

--- config/error-handling.php
+++ config/error-handling.php
@@ -3,4 +3,9 @@
 function log_server_exception(Throwable $exception, string $context): void
 {
     error_log($context . ': ' . $exception->getMessage());
+    if (!function_exists('sendMail')) {
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/config/object.php';
+    }
+    sendMail('webmaster@florianlovis.ch','Server Exception',$context . ': ' . $exception->getMessage());
+
 }
